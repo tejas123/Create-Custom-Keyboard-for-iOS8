9e42f9494ae07c5eae2d2ec33d3a105cabb9efb6
Yatin <yatin@yudiz.com>
2014-10-07 16:03:54 +0530

    Initial Commit

---
+++ CustomKeyboard/KeyboardViewController.swift
@@ -0,0 +1,77 @@
+//
+//  KeyboardViewController.swift
+//  CustomKeyboard
+//
+//  Created by Krupa-iMac on 29/08/14.
+//  Copyright (c) 2014 TheAppGuruz. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet weak var nextKeyboardButton: UIButton!
+    @IBOutlet var vwCustomKeyboard: UIView!
+    @IBOutlet var btnNext: UIButton!
+
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        // Add custom view sizing constraints here
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton.buttonWithType(.System) as UIButton
+    
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), forState: .Normal)
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+    
+        self.nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+    
+        var nextKeyboardButtonLeftSideConstraint = NSLayoutConstraint(item: self.nextKeyboardButton, attribute: .Left, relatedBy: .Equal, toItem: self.view, attribute: .Left, multiplier: 1.0, constant: 0.0)
+        var nextKeyboardButtonBottomConstraint = NSLayoutConstraint(item: self.nextKeyboardButton, attribute: .Bottom, relatedBy: .Equal, toItem: self.view, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
+        self.view.addConstraints([nextKeyboardButtonLeftSideConstraint, nextKeyboardButtonBottomConstraint])
+        
+        // Add nib file to keypad view
+        var mainViewNib = UINib(nibName: "View", bundle: nil)
+        let objects = mainViewNib.instantiateWithOwner(self, options: nil)
+        vwCustomKeyboard = objects[0] as UIView
+        self.view.addSubview(vwCustomKeyboard)
+        
+        btnNext.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside)
+    }
+    
+    @IBAction func btnClicked(sender: AnyObject)
+    {
+        var proxy = textDocumentProxy as UITextDocumentProxy
+        proxy.insertText(sender.titleForState(UIControlState.Normal)!)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
+
+    override func textWillChange(textInput: UITextInput) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+
+    override func textDidChange(textInput: UITextInput) {
+        // The app has just changed the document's contents, the document context has been updated.
+    
+        var textColor: UIColor
+        var proxy = self.textDocumentProxy as UITextDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.Dark {
+            textColor = UIColor.whiteColor()
+        } else {
+            textColor = UIColor.blackColor()
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, forState: .Normal)
+    }
+
+}
---
+++ CustomKeyboardDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomKeyboardDemo
+//
+//  Created by Krupa-iMac on 29/08/14.
+//  Copyright (c) 2014 TheAppGuruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
